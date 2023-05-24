941f1a585389b1aa921900cc45cacaf7ca74a8c3
Ben <sturgeonkid@gmail.com>
2023-05-24 10:06:12 +0200

    Fixed comment section colouring

--- components/CommentForm.vue
+++ components/CommentForm.vue
@@ -1,62 +1,60 @@
 <template>
-  <v-container class="grey lighten-5" md="10" lg="11" >
-    <div v-if="!personal" >
-    <H1 class="header">Comments</H1>
-    <h2 class="mt-0 mb-1 pa-4 pt-0">
-      Comments, questions, thoughts?<br />
-      Share them below:
-    </h2>
-    <v-form ref="form" v-model="form" class="">
-      <v-row>
-        <v-col cols="12" class="form_container pt-0 pl-8">
-          <h3>Your name:</h3>
+  <v-container class="formBackground" md="10" lg="11">
+    <div v-if="!personal">
+      <h2 class="mt-0 mb-1 pb-5 pa-4 pt-0">
+        Comments, questions, thoughts?<br />
+        Share them below:
+      </h2>
+      <v-form ref="form" v-model="form" class="formSheet">
+        <v-row>
+          <v-col cols="12" class="form_container pt-5 pl-8">
+            <h3>Your name:</h3>
 
-          <v-text-field
-            class="form"
-            width="100px"
-            v-model="name"
-            solo
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12" class="formContainer pt-0 pl-8">
-          <h3 class="formEmail">
-            Your email: <em>(Private. For my eyes only)</em>
-          </h3>
+            <v-text-field
+              class="form"
+              width="100px"
+              v-model="name"
+              solo
+              :rules="[rules.length(1)]"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" class="form_container pt-0 pl-8">
+            <h3>Your email:</h3>
 
-          <v-text-field
-            class="form"
-            v-model="email"
-            solo
-            :rules="[rules.email]"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12" md="10" lg="8" class="pt-0 pl-8">
-          <h3 class="mt-0">Your words:</h3>
-          <v-textarea
-            class="text-wrap"
-            filled
-            height="150px"
-            v-model="comment"
-            solo
-            :rules="[rules.length(1)]"
-          ></v-textarea>
-        </v-col>
-      </v-row>
-    </v-form>
-    <v-row>
-      <v-btn
-        class="formButton ma-4 mt-0 ml-8"
-        @click="writeUserData()"
-        :disabled="!form"
-      >
-        Submit comment
-      </v-btn>
-    </v-row>
-  </div>
+            <v-text-field
+              class="form"
+              v-model="email"
+              solo
+              :rules="[rules.email]"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="10" lg="8" class="pt-0 pl-8">
+            <h3>Your words:</h3>
+            <v-textarea
+              class="text-wrap"
+              filled
+              height="150px"
+              v-model="comment"
+              solo
+              :rules="[rules.length(1)]"
+            ></v-textarea>
+          </v-col>
+        </v-row>
+        <!-- <v-row> -->
+        <v-btn
+          class="formButton ma-5 mt-3"
+          @click="writeUserData()"
+          :disabled="!form"
+        >
+          Submit comment
+        </v-btn>
+        <!-- </v-row> -->
+      </v-form>
+    </div>
   </v-container>
 </template>
 
@@ -64,7 +62,7 @@
 import { getDatabase, ref, set, onValue } from "firebase/database";
 
 export default {
-  props: ['personal'],
+  props: ["personal"],
   name: "AppHeader",
   data: function () {
     return {
@@ -86,7 +84,7 @@ export default {
   },
   created() {
     const db = getDatabase();
-    const pageRef = ref(db, "pageRefs/" );
+    const pageRef = ref(db, "pageRefs/");
     onValue(pageRef, (snapshot) => {
       snapshot.forEach((childSnapshot) => {
         if (childSnapshot.val() == this.slug) {
@@ -110,7 +108,7 @@ export default {
       var time =
         today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
       var dateTime = date + " " + time;
-      set(ref(db, "pages/" +this.pageId + "/" + id), {
+      set(ref(db, "pages/" + this.pageId + "/" + id), {
         name: this.name,
         email: this.email,
         comment: this.comment,
@@ -165,23 +163,31 @@ export default {
   margin-bottom: 1px;
   padding-bottom: 1px;
 }
-.formContainer {
-  padding-left: 10px;
 
-  margin-top: 1px;
-  margin-bottom: 1px;
-  padding-bottom: 1px;
-  padding-top: 1px;
+.formSheet {
+  background-color: #1b1d1e;
 }
+.formBackground {
+  background-color: #363636;
+}
+
 .formEmail em {
   color: grey;
   font-style: normal;
 }
 
-.formButton {
+::v-deep .theme--light.v-btn.formButton {
   font-family: "valkyrieC4";
   font-weight: 400;
   color: white;
   background-color: #616161 !important;
+  /* padding-top: 15px; */
+  margin-top: 10px;
 }
+/* ::v-deep .theme--light.v-btn.formButton[disabled] {
+  color: whitesmoke !important;
+  background-color: #616161 !important;
+
+  margin-top: 10px;
+} */
 </style>
